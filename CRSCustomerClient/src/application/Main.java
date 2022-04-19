074254c7d56db3a3050207a6c2b38942a6c35b7b
AbizarLeman <56353860+AbizarLeman@users.noreply.github.com>
2022-04-19 21:19:18 +0800

    Login fix.

--- CRSCustomerClient/src/application/Main.java
+++ CRSCustomerClient/src/application/Main.java
@@ -1,24 +1,44 @@
 package application;
 	
+import java.io.IOException;
+
 import javafx.application.Application;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.fxml.FXMLLoader;
 
 
 public class Main extends Application {
+    private static Stage stage;
+    
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
+			Pane root = (Pane)FXMLLoader.load(getClass().getResource("CustomerClient.fxml"));
+			stage = primaryStage;
+			
+			Scene scene = new Scene(root, 600, 600);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			
+			primaryStage.setTitle("Car Rental System - User Login");
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
 	}
+	
+    public void changeScene(String fxml) throws IOException {
+        Parent box = FXMLLoader.load(getClass().getResource(fxml));
+        stage.getScene().setRoot(box);
+    }
+    
+    public Stage getStage() throws IOException {
+        return stage;
+    }
 	
 	public static void main(String[] args) {
 		launch(args);
